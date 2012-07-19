1c3b3bb790a0cf100e9c26e7a58bff3fc638edf2
rsouverain <rsouverain@kaliop.com>
2012-07-19 12:37:03 +0200

    github migration

---
+++ Settings/user-keymap.js
@@ -0,0 +1,7 @@
+[
+	{ "keys": ["ctrl+b"], "command": "toggle_side_bar" },
+	{ "keys": ["ctrl+d"], "command": "run_macro_file", "args": {"file": "Packages/Default/Delete Line.sublime-macro"} },
+	{ "keys": ["ctrl+shift+d"], "command": "duplicate_line" },
+	{ "keys": ["ctrl+shift+c"], "command": "duplicate_line" },
+	{ "keys": ["ctrl+alt+c"], "command": "duplicate_line" }
+]
---
+++ Settings/user-settings.js
@@ -0,0 +1,14 @@
+{
+	"auto_complete_size_limit": 4194304,
+	"color_scheme": "Packages/Color Scheme - Default/LethaK.tmTheme",
+	"detect_indentation": false,
+	"font_size": 10.5,
+	"indent_guide_options":
+	[
+		"draw_normal",
+		"draw_active"
+	],
+	"word_separators": "./\\()\"'-:,.;<>~!@#%^&*|+=[]{}`~?",
+	"word_wrap": true,
+	"default_line_ending": "unix",
+}
